5d9aa655d3476ca263518c5eb97f405427ecc907
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-04-09 09:05:49 +0530

    Update server.js to connect to MongoDB Atlas

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/businessReels");
+// 🔥 ROOT ROUTE
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
 
+// 🔥 MongoDB Atlas
+mongoose.connect("YOUR_MONGODB_ATLAS_URL")
+  .then(()=>console.log("MongoDB Connected ✅"))
+  .catch(err=>console.log(err));
+
+// ROUTES
 app.use("/auth", require("./routes/auth"));
 app.use("/reels", require("./routes/reels"));
 
-app.listen(5000, () => console.log("Server running"));
+// PORT FIX
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));

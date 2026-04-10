765760373d4e54a44576404f0255d950bd9d9236
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-04-10 16:14:03 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,11 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("CLOUDINARY ENV:", {
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET ? "YES" : "NO"
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,

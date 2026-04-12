4fec9aad18e86ce3f7df81240ad223b1016ea5e8
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-04-12 16:12:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/ai", aiRoutes);
+app.use("/music", require("./routes/music"));
 // ================== UPLOADS FOLDER (LOCAL BACKUP) ==================
 const uploadPath = path.join(__dirname, "uploads");
 
